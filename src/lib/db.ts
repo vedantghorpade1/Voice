b87c8b83712b1d6aa22ffb18c038e51979ff747a
vedantghorpade1 <vedantghorpade50@gmail.com>
2025-10-19 23:02:38 +0530

    yes

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,43 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
+if (!MONGODB_URI || MONGODB_URI.length === 0) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
-let cached: { conn: any; promise: any } = (global as any).mongoose;
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseCache = (global as any).mongoose;
 
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
+  // If a connection is already cached, return it
   if (cached.conn) {
     return cached.conn;
   }
 
+  // If a connection promise doesn't exist, create it
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI, opts);
   }
+
+  // Await the promise and cache the connection object
   cached.conn = await cached.promise;
   return cached.conn;
 }
